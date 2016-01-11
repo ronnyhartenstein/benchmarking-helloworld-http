ecb0b3d800a2d9c426ce4dd0d0b198a109e812d5
Ronny Hartenstein <ronny@rh-flow.de>
2016-01-11 21:47:35 +0100

    update phoenix and remove all plugs

--- phoenix2/web/router.ex
+++ phoenix2/web/router.ex
@@ -1,26 +1,8 @@
 defmodule Phoenix2.Router do
   use Phoenix2.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", Phoenix2 do
-    pipe_through :browser # Use the default browser stack
-
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Phoenix2 do
-  #   pipe_through :api
-  # end
 end
